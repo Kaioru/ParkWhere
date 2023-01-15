49920177b2b825ea9d769eabb0ce74a8ccda7210
Kaioru <keith@kaioru.co>
2023-01-16 00:46:16 +0800

    Format code

--- lib/tabs/carparks.dart
+++ lib/tabs/carparks.dart
@@ -44,89 +44,95 @@ class _CarparksState extends State<Carparks> {
               child: Icon(CupertinoIcons.search)),
         ),
         SliverFillRemaining(
-            child: FutureBuilder(future: Future<dynamic>(() async {
-          final response = await http.get(
-              Uri.parse(
-                  'http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2'),
-              headers: {"AccountKey": "Io54g7V1TSWPZuswdOhHOw=="});
+          child: FutureBuilder(future: Future<dynamic>(() async {
+            final response = await http.get(
+                Uri.parse(
+                    'http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2'),
+                headers: {"AccountKey": "Io54g7V1TSWPZuswdOhHOw=="});
 
-          return jsonDecode(response.body)["value"].where((v) => state.pins.contains(v["CarParkID"].toString())).toList();
-        }), builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return CupertinoActivityIndicator();
-          }
+            return jsonDecode(response.body)["value"]
+                .where((v) => state.pins.contains(v["CarParkID"].toString()))
+                .toList();
+          }), builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return CupertinoActivityIndicator();
+            }
 
-          if (snapshot.data.length == 0) {
-            return Center(child: Text("No pinned carparks!"));
-          }
+            if (snapshot.data.length == 0) {
+              return Center(child: Text("No pinned carparks!"));
+            }
 
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                  CupertinoPageRoute<void>(
-                    builder: (BuildContext context) {
-                      return CupertinoPageScaffold(
-                          navigationBar: CupertinoNavigationBar(
-                            middle: Text(snapshot.data[index]["Development"]),
-                          ),
-                          child: CarparksDetail(state: state, details: snapshot.data[index]));
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        CupertinoPageRoute<void>(
+                          builder: (BuildContext context) {
+                            return CupertinoPageScaffold(
+                                navigationBar: CupertinoNavigationBar(
+                                  middle:
+                                      Text(snapshot.data[index]["Development"]),
+                                ),
+                                child: CarparksDetail(
+                                    state: state,
+                                    details: snapshot.data[index]));
+                          },
+                        ),
+                      ).then((_) => setState(() {}));
                     },
-                  ),
-                ).then((_) => setState(() {}));
-                  },
-                  child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Icon(state.pins.contains(snapshot.data[index]
-                                          ["CarParkID"]
-                                      .toString())
-                                  ? CupertinoIcons.pin_fill
-                                  : CupertinoIcons.pin),
-                              Expanded(
-                                  child: Text(
-                                snapshot.data[index]["Development"]
-                                    .toString()
-                                    .toTitleCase(),
+                    child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Icon(state.pins.contains(snapshot.data[index]
+                                            ["CarParkID"]
+                                        .toString())
+                                    ? CupertinoIcons.pin_fill
+                                    : CupertinoIcons.pin),
+                                Expanded(
+                                    child: Text(
+                                  snapshot.data[index]["Development"]
+                                      .toString()
+                                      .toTitleCase(),
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ))
+                              ],
+                            ),
+                            Text(
+                                snapshot.data[index]["AvailableLots"]
+                                        .toString() +
+                                    " available lots!",
                                 style: TextStyle(
-                                    fontSize: 18.0,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ))
-                            ],
-                          ),
-                          Text(
-                              snapshot.data[index]["AvailableLots"].toString() +
-                                  " available lots!",
-                              style: TextStyle(
-                                  fontSize: 12.0, color: Colors.black)),
-                        ],
-                      ),
-                      height: 80.0,
-                      padding: EdgeInsets.all(13.0),
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 18.0, vertical: 6.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(13.0),
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 10.0,
-                            offset: Offset(0.0, 10.0),
-                          ),
-                        ],
-                      )));
-            },
-          );
-        }),
+                                    fontSize: 12.0, color: Colors.black)),
+                          ],
+                        ),
+                        height: 80.0,
+                        padding: EdgeInsets.all(13.0),
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 18.0, vertical: 6.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(13.0),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 10.0,
+                              offset: Offset(0.0, 10.0),
+                            ),
+                          ],
+                        )));
+              },
+            );
+          }),
         )
       ],
     ));
